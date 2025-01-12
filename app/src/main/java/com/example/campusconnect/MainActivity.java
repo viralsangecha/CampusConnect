b5d786b35a51c35f421ac6d85110384274ab75fc
viralsangecha <viralsangecha03@gmail.com>
2025-01-12 12:40:48 +0530

    range radius reduce

--- app/src/main/java/com/example/campusconnect/MainActivity.java
+++ app/src/main/java/com/example/campusconnect/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
+        NotificationScheduler.scheduleDailyNotification(this);
         if (!isNetworkAvailable()) {
             // Redirect to No Internet Activity
             Intent intent = new Intent(this, NoInternetActivity.class);
@@ -98,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        NotificationScheduler.scheduleDailyNotification(this);
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
